f635e7a0416029735bba6c195bfaedc6a6a3a60e
ceyhu <muradogluceyhun@gmail.com>
2023-10-19 19:30:18 +0300

    update5

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='afiedolsun-migros',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ceyhu',
+    author_email='',
+    description=''
+)
